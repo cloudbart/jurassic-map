54e6acba6ef2e00d8938f2f97bf928be10316ba9
cloudbart <pibrocher@gmail.com>
2022-11-30 00:28:28 +0000

    updated API key

--- jurassic-map-react/src/aws-exports.js
+++ jurassic-map-react/src/aws-exports.js
@@ -6,7 +6,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://besjdurbyfgzpbkph7wclnfkc4.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-b5rzg3bm4jb2phm5w3pndpz64a"
+    "aws_appsync_apiKey": "da2-xq5ufevwxnb4zdaeopz3vm3rbq"
 };
 
 
