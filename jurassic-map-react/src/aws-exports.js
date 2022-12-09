5833def43f0871629be1cc8cb4bb97f134096328
cloudbart <pibrocher@gmail.com>
2022-12-09 19:26:09 +0000

    change to api.jurassicmap.com

--- jurassic-map-react/src/aws-exports.js
+++ jurassic-map-react/src/aws-exports.js
@@ -3,7 +3,7 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_appsync_graphqlEndpoint": "https://nto2dgwogzfa7iqnkowxtc7jl4.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://api.jurassicmap.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
     "aws_appsync_apiKey": "da2-dlic6kr4tve7veq2b7y7nrgr5m"
